1aa75b5902c2c73c00d60ba05ffe9fe89d2e1101
frank-martinez-27 <frank.martinez.27@outlook.com>
2017-11-13 16:22:53 -0800

    Create Course feature complete with Redux

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import * as courseActions from '../actions/courseActions';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
@@ -10,18 +12,22 @@ class CoursesPage extends React.Component {
     this.onTitleChange = this.onTitleChange.bind(this);
     this.onClickSave = this.onClickSave.bind(this);
   }
-  onTitleChange(e){
-    const course=this.state.course;
-    course.title=e.target.value;
-    this.setState({course:course});
+  onTitleChange(e) {
+    const course = this.state.course;
+    course.title = e.target.value;
+    this.setState({ course: course });
   }
-  onClickSave(){
-    alert(`Saving ${this.state.course.title}`);
+  onClickSave() {
+    this.props.dispatch(courseActions.createCourse(this.state.course));
+  }
+  courseRow(course,idx){
+    return <div key={idx}>{course.title}</div>;
   }
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
         <hr />
         <h2>Add Course</h2>
         <input
@@ -39,5 +45,10 @@ class CoursesPage extends React.Component {
     );
   }
 }
+function mapStateToProps(state, ownProps) {
+  return {
+    courses: state.courses //state.courses is the same name as in the reducer's index.js
+  };
+}
 
-export default CoursesPage;
+export default connect(mapStateToProps)(CoursesPage);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import 'babel-polyfill';
 import * as React from 'react';
 import { render } from 'react-dom';
-// import { Router, browserHistory } from 'react-router';
+import configureStore from './components/store/configureStore';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
+
+import { Provider } from 'react-redux';
+
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './styles/sb-admin.css';
 import './styles/plugins/morris.css';
 import './styles/font-awesome/css/font-awesome.min.css';
 
+const store = configureStore();
+
 render(
-  <Router>
-    <App />
-  </Router>, document.getElementById('app')
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
+  , document.getElementById('app')
 );
